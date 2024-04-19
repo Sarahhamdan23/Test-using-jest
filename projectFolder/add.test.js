537a4086362d4ff101486cc1fca5fd117be6e68a
Sarah <sarahwbas@gmail.com>
2024-04-19 17:47:02 -0500

    add function added

--- projectFolder/add.test.js
+++ projectFolder/add.test.js
@@ -5,3 +5,22 @@ test('<What the test case does>', () => {
     const expectedResult = 8;
     expect(fnResult).toBe(expectedResult);
 });
+
+
+
+
+
+
+function add(a, b) {
+    return a + b;
+  }
+  
+
+const { add } = require('./mathFunctions');
+
+test('adds 1 + 2 to equal 3', () => {
+  
+  const result = add(1, 2);
+  const expectedResult = 3;
+  expect(result).toBe(expectedResult);
+});
